ed1fa12e9b8503e0bae818780355396a0a9dd623
scootgewp <diegoazael732@gmail.com>
2020-10-28 10:38:28 -0600

    Mista nerfs

--- chars/Mista_G/amaAI_unotag/mista.cmd
+++ chars/Mista_G/amaAI_unotag/mista.cmd
@@ -87,6 +87,51 @@ command = ~D, DF, F
 time = 20
 
 [command]
+name = "aimyouidiots"
+command = ~D, DF, F, x+y
+time = 16
+
+[command]
+name = "aimyouidiots"
+command = ~D, DF, F, x+z
+time = 16
+
+[command]
+name = "aimyouidiots"
+command = ~D, DF, F, z+y
+time = 16
+
+[command]
+name = "gettowork"
+command = ~D, DB, B, x+y
+time = 16
+
+[command]
+name = "gettowork"
+command = ~D, DB, B, x+z
+time = 16
+
+[command]
+name = "gettowork"
+command = ~D, DB, B, z+y
+time = 16
+
+[command]
+name = "rollingstone"
+command = ~B, DB, D, DF,F, x+y
+time = 16
+
+[command]
+name = "rollingstone"
+command = ~B, DB, D, DF,F, x+z
+time = 16
+
+[command]
+name = "rollingstone"
+command = ~B, DB, D, DF,F, z+y
+time = 16
+
+[command]
 name = "qcb"
 command = ~D, DB, B
 time = 20
@@ -567,10 +612,11 @@ value = 2900
 triggerall = var(59)<=0&& RoundState = 2
 triggerall = NumHelper(1125) = 0
 triggerall = NumHelper(1401) = 0
-triggerall = power >= 1000
+triggerall = power >= 1000 && command = "aimyouidiots"
 triggerall = var(55) != 4
 triggerall = !Ishelper
 triggerall = statetype != A
+;triggerall = 
 triggerall = numhelper(10371)
 triggerall = helper(10371),var(40)
 triggerall = (helper(10371),var(0) = [1,3]) || (helper(10371),var(1) = [1,3]) 
@@ -1052,8 +1098,6 @@ triggerall = numhelper(10371)
 triggerall = (helper(10371),var(1) = [1,3]) 
 triggerall = statetype != A
 trigger1 = ctrl
-trigger2 = (stateno = 200) && movecontact
-trigger3 = (stateno = 400) && movecontact
 
 [State -1, —§‚¿‹­]
 type = ChangeState
@@ -1064,10 +1108,6 @@ triggerall = numhelper(10371)
 triggerall = (helper(10371),var(2) = [1,3]) 
 triggerall = statetype != A
 trigger1 = ctrl
-trigger2 = (stateno = 200) && movecontact
-trigger3 = (stateno = 210) && movecontact
-trigger4 = (stateno = 400) && movecontact
-trigger5 = (stateno = 410) && movecontact
 trigger6 = stateno = 105
 
 
@@ -1184,7 +1224,7 @@ value = 4000
 triggerall = var(59) = 1 && RoundState = 2
 triggerall = NumHelper(4003) = 0
 triggerall = power >= 1000
-triggerall = statetype != A
+triggerall = statetype != A && command = "rollingstone" 
 triggerall= EnemyNear,StateType != L
 triggerall= EnemyNear,MoveType != H
 triggerall= EnemyNear,MoveType = A
