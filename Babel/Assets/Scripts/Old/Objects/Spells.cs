535d8c28915dc06a00056daf29a69997b19e16fe
RNGSSB <59853617+RNGSSB@users.noreply.github.com>
2020-12-06 22:02:55 -0600

    w

--- Babel/Assets/Scripts/Old/Objects/Spells.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu (fileName ="New Spell", menuName ="Spell")]
-public class Spells : Item
-{
-    public int manacost;
-
-}
